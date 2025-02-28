d5f70319fc7465060b3415ee9285dcd8be5451dc
Deepika <Deepika@DeepikaRajesh>
2025-02-28 13:21:47 +0530

    added ele3

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Leads.java
@@ -0,0 +1,11 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Leads {
+	
+	@FindBy(xpath="//img[@title=''Create Lead...]")
+	private WebElement ele3;
+
+}
